a7a30def900254c7ccd3f46a16b6ad3b131c1110
GraemeBradbury <graeme@siliconshark.com>
2025-09-24 17:54:53 +0100

    refactored tests

--- src/battle.spec.ts
+++ src/battle.spec.ts
@@ -7,48 +7,40 @@ const getCharactersMocked = getCharacters as jest.MockedFunction<
   typeof getCharacters
 >;
 
-it("battle should return the hero if they have a higher score", async () => {
-  getCharactersMocked.mockReturnValue(
-    Promise.resolve({
-      items: [
-        { name: "Winner", score: 9.0, type: "hero" },
-        { name: "Loser", score: 8.0, type: "villain" },
-      ],
-    })
-  );
-
-  const result = await battleWithGetCharacters("Winner", "Loser");
-
-  expect(result).toEqual({
-    name: "Winner",
-    score: 9.0,
-    type: "hero",
-  });
-});
-
 describe("battle", () => {
-  const expectedVictor = {
-    name: "Batman",
-    score: 8.3,
+  const hero = {
+    name: "Hero",
+    score: 2.5,
     type: "hero",
-    weakness: "Joker",
+  };
+  const villain = {
+    name: "Villain",
+    score: 2.0,
+    type: "villain",
   };
 
-  it("with weakness", async () => {
-    const getCharactersWithWeaknessMocked = (): Promise<CharactersResponse> => {
-      return Promise.resolve({
-        items: [
-          expectedVictor,
-          {
-            name: "Joker",
-            score: 8.2,
-            type: "villain",
-          },
-        ],
-      });
-    };
+  it("should return the hero if they have a higher score", async () => {
+    getCharactersMocked.mockReturnValue(
+      Promise.resolve({
+        items: [hero, villain],
+      })
+    );
 
-    const victor = await battle(getCharactersWithWeaknessMocked, "Batman", "Joker");
-    expect(victor).toEqual(expectedVictor);
+    const result = await battleWithGetCharacters(hero.name, villain.name);
+
+    expect(result).toEqual(hero);
+  });
+
+  it("should return the villain if the hero has a weakness to them", async () => {
+    const weakHero = { ...hero, weakness: villain.name };
+    getCharactersMocked.mockReturnValue(
+      Promise.resolve({
+        items: [weakHero, villain],
+      })
+    );
+
+    const result = await battleWithGetCharacters(weakHero.name, villain.name);
+
+    expect(result).toEqual(villain);
   });
 });
