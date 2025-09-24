3d56a541c12208dd09c097fe20119847e4912b38
GraemeBradbury <graeme@siliconshark.com>
2025-09-24 18:05:25 +0100

    implement weakness check

--- src/battle.ts
+++ src/battle.ts
@@ -1,5 +1,11 @@
 import { getCharacters, Character, CharactersResponse } from "./getCharacters";
 
+function scoreWhenFighting(h: Character, v: Character): number {
+  const weaknessModifier = h.weakness != null && h.weakness == v.name ? 1 : 0;
+
+  return h.score - weaknessModifier;
+}
+
 export async function battle(
   getCharacters: () => Promise<CharactersResponse>,
   heroName: string,
@@ -14,10 +20,12 @@ export async function battle(
     };
   }
 
-  const hero = characters.items.find((e) => e.name === heroName);
-  const villain = characters.items.find((e) => e.name === villainName);
+  let hero = characters.items.find((e) => e.name === heroName);
+  hero = hero ?? { name: "", score: 0.0, type: "hero" };
+  let villain = characters.items.find((e) => e.name === villainName);
+  villain = villain ?? { name: "", score: 0.0, type: "villain" };
 
-  return hero!.score >= villain!.score ? hero! : villain!;
+  return scoreWhenFighting(hero, villain) >= villain.score ? hero : villain;
 }
 
 export const battleWithGetCharacters = (
